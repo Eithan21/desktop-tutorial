21067df671422fa900aced044492b1db448f62ef
David Kcy <109700765+Eithan21@users.noreply.github.com>
2023-12-19 01:01:41 -0400

    commit

--- Parcial/angular-monorepo/apps/appY/src/app/app.component.ts
+++ Parcial/angular-monorepo/apps/appY/src/app/app.component.ts
@@ -1,16 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { Menu2Component } from '@angular-monorepo/menu2';
+import { PostsService } from './services/posts/posts.service';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule,Menu2Component],
+  imports: [NxWelcomeComponent, RouterModule,Menu2Component,CommonModule],
   selector: 'angular-monorepo-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'appY';
+  Posts : any; 
+  constructor(
+    private PostsService: PostsService, 
+    private router: Router,) {
+   }
+  ngOnInit() {
+    this.webservicegetPosts();
+  }
+  public webservicegetPosts(){
+    this.PostsService.getPosts().subscribe(data=> {
+      this.Posts = data;
+      console.log(data);
+
+    })
+  }
 }
---
+++ Parcial/angular-monorepo/apps/appY/src/app/services/posts/posts.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+
+  
+    public baseUrl = "https://jsonplaceholder.typicode.com/posts";
+
+    constructor(private httpClient: HttpClient) { 
+    }
+    public getPosts(): Observable<any> {
+      return this.httpClient.get(this.baseUrl);
+    }
+  
+}
