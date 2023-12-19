63e3ed0cefe524b105d583b4d2bd5855a8595f84
David Kcy <109700765+Eithan21@users.noreply.github.com>
2023-12-19 01:08:55 -0400

    commit

--- Parcial/angular-monorepo/apps/appZ/src/app/app.component.ts
+++ Parcial/angular-monorepo/apps/appZ/src/app/app.component.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { NavComponent } from '@angular-monorepo/nav';
+import { CommentsService } from './services/comments/comments.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, NavComponent],
+  imports: [NxWelcomeComponent, RouterModule, NavComponent, CommonModule],
   selector: 'angular-monorepo-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'appZ';
+  Comments : any; 
+  constructor(
+    private CommentsService: CommentsService, 
+    private router: Router,) {
+   }
+  ngOnInit() {
+    this.webservicegetPosts();
+  }
+  public webservicegetPosts(){
+    this.CommentsService.getPosts().subscribe(data=> {
+      this.Comments = data;
+      console.log(data);
+
+    })
+  }
 }
---
+++ Parcial/angular-monorepo/apps/appZ/src/app/services/comments/comments.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentsService {
+
+    public baseUrl = "https://jsonplaceholder.typicode.com/comments";
+    constructor(private httpClient: HttpClient) { 
+    }
+    public getPosts(): Observable<any> {
+      return this.httpClient.get(this.baseUrl);
+    }
+  
+}
